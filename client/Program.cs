ae9d40e9caf2018cb275d465e4ec9c41110f6605
Sam Wronski <sam@worldofzero.com>
2023-02-09 07:22:11 -0800

    Add client to consume our API using NSwag codegen

---
+++ client/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using client;
+
+Console.WriteLine("Rolling Some Dice");
+swaggerClient client = new("https://localhost:7168/", new());
+var result = await client.RollDiceAsync("6d6");
+Console.WriteLine($"Result: {result.Total}");
